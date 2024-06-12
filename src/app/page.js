bf4d803db114e384fab7017c43bcd813b81d7cd5
mmh-masud03 <2054901003@student.bup.edu.bd>
2024-06-12 21:34:13 +0600

    animating background image

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,11 @@
 import styles from "./page.module.css";
 import WavyCircle from "@/components/wavy circle loader/WavyCircle";
-import BackgroundImage from "@/components/background image/BackgroundImage";
+import BackgroundImage from "../components/background image/BackgroundImage";
 
 export default function Home() {
   return (
     <div>
-      <backgroundImage />
+      <BackgroundImage />
       <WavyCircle />
     </div>
   );
--- src/components/background image/BackgroundImage.js
+++ src/components/background image/BackgroundImage.js
@@ -1,9 +1,26 @@
-import React from "react";
-import "./BackgroundImage.module.css";
+"use client";
+import React, { useEffect, useRef } from "react";
+import styles from "./BackgroundImage.module.css";
+
 function BackgroundImage() {
+  const bannerRef = useRef(null);
+
+  useEffect(() => {
+    const banner = bannerRef.current;
+    for (let i = 0; i < 400; i++) {
+      const block = document.createElement("div");
+      block.classList.add(styles.blocks);
+      block.style.animationDelay = `${i * 0.05}s`;
+      banner.appendChild(block);
+    }
+  }, []);
+
   return (
-    <div className="wrapper">
-      <h2>Background Image</h2>
+    <div className={styles.wrapper}>
+      <section>
+        <h2>Background Image</h2>
+        <div className={styles.banner} ref={bannerRef}></div>
+      </section>
     </div>
   );
 }
